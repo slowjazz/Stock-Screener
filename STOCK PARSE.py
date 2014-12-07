2480270b4a1ee6e903403fde4a91cc18e3b5a579
Justin Shetty <jas0504@gmail.com>
2014-12-07 13:57:06 -0500

    Data retrieval functioning
    Program capable of pulling financial data for single stock ticker

--- STOCK PARSE.py
+++ STOCK PARSE.py
@@ -5,15 +5,11 @@ stock=raw_input("Please enter at least one stock ticker symbol : ")
 
 ref="http://finance.yahoo.com/d/quotes.csv?s="+str(stock)+"&f=pr"
 
-print ref
+str(ref)
 
-# RETRIEVE CSV FILE
-#with open('quote.csv', 'rb') as csvfile:
-#   data = csv.reader(csvfile, delimiter=' ', quotechar='|')
-#    for row in data:
-#         print ', '.join(row)
+urllib.urlretrieve(ref,"quotes.csv")
 
-data = csv.reader(open('quote.csv', 'rb'))
+data = csv.reader(open('quotes.csv', 'rb'))
 for row in data:
     price = row[0]
     e = row[1]
---
+++ test/test.py
@@ -0,0 +1,2 @@
+import urllib
+urllib.urlretrieve("http://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg","Red_Apple.jpg")
